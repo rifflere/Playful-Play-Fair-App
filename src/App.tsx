ba936d582b49526d398193beb2ffbba012be0a2d
rifflere <82406585+rifflere@users.noreply.github.com>
2026-03-24 17:45:56 -0700

    Added use state to app to manage letter grid and cipher key, passes to Arrange Column as props

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import './App.css'
 import EncryptColumn from './components/EncryptColumn';
 import ArrangeColumn from './components/ArrangeColumn';
 import DecryptColumn from './components/DecryptColumn';
+import { useState } from 'react';
 import { AppBar, Toolbar, Box} from '@mui/material';
 
+const alphabet = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 function App() {
+  const [cipherKey, setCipherKey] = useState<string[]>([]);
+  const remaining = alphabet.filter(l => ! cipherKey.includes(l))
+  const fullGrid = [...cipherKey, ...remaining]
+
   return (
     <>
     <AppBar position="static">
       <Toolbar sx={{ bgcolor:'RebeccaPurple' }}></Toolbar>
     </AppBar>
 
-      <Box sx={{ bgcolor:'purple', display:'flex', flexDirection:'row' }}>
+    <Box sx={{ bgcolor:'purple', display:'flex', flexDirection:'row' }}>
         <EncryptColumn />
-        <ArrangeColumn />
+        <ArrangeColumn fullGrid={fullGrid} setCipherKey={setCipherKey} />
         <DecryptColumn />
       </Box>
     </>
--- src/components/ArrangeColumn.tsx
+++ src/components/ArrangeColumn.tsx
@@ -1,16 +1,19 @@
 import { Container, Paper, Typography } from '@mui/material';
 import Letter from './Letter';
 
-function ArrangeColumn() {
-    const letters = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  
+interface ArrangeColumnProps {
+  fullGrid: string[];
+  setCipherKey: (key: string[]) => void;
+}
+
+function ArrangeColumn({ fullGrid, setCipherKey }: ArrangeColumnProps) {
     return (
     <Container sx={{ flex: 1 }}>
         <Paper elevation={2}>
         <Typography variant="h2">Arrange</Typography>
         <Typography variant="body1">Arrange grid here</Typography>
         <Container sx={{ display: 'flex', flexWrap: 'wrap' }}>
-            {letters.map((char) => (
+            {fullGrid.map((char) => (
                 <Letter key={char} char={char}/>
             ))}
         </Container>

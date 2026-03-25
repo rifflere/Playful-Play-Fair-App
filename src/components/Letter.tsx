a7d247bb792086cf3c491195ce26ffc86b9b3c66
rifflere <82406585+rifflere@users.noreply.github.com>
2026-03-24 22:32:01 -0700

    Make each letter sortable using dnd-kit

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,12 +1,23 @@
 import { Paper } from '@mui/material';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 interface LetterProps {
     char : string;
 }
 
 function Letter({ char } : LetterProps) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: char});
     return (
-        <Paper elevation={2} sx={{ width: '20%' }}>
+        <Paper 
+            ref={setNodeRef} 
+            {...attributes} 
+            {...listeners} 
+            elevation={2} 
+            sx={{ width: '20%',
+                transform: CSS.Transform.toString(transform),
+                transition,
+         }}>
             {char}
         </Paper>
     );
